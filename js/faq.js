2b23380f9f7264d9024e46ad692893e649532042
santiago-leticia <santiagoleticia68@gmail.com>
2025-05-25 20:25:49 -0300

    dia25parte1

--- js/faq.js
+++ js/faq.js
@@ -22,19 +22,3 @@ function clicou(){
     console.log(duvida)
 
 
-    const form = document.getElementById('cemail');
-    const campos= document.querySelectorAll('.required');
-    const spans = document.querySelectorAll('.span-required');
-    function setError(FAQ){
-        campos[FAQ].computedStyleMap
-    }
-    function nameValidade(){
-        if(campos[0].value.length==0){
-            setError[0]
-        }
-        else{
-            let texto= texto.getElementById('ctexto')
-            console.log(texto)
-        }
-    
-    }
